8be565e1aef30f3a50025d3a521459bc8d2f73d8
Cybervitexus <info@vitexsoftware.cz>
2018-10-21 00:18:54 +0200

    example query for in/out payments added

--- modules/WaitingIncome.php
+++ modules/WaitingIncome.php
@@ -8,10 +8,16 @@
  *
  * @author vitex
  */
-class WaitingIncome  extends \FlexiPeeHP\DigestMail\DigestModule implements \FlexiPeeHP\DigestMail\DigestModuleInterface
+class WaitingIncome extends \FlexiPeeHP\DigestMail\DigestModule implements \FlexiPeeHP\DigestMail\DigestModuleInterface
 {
+
     public function heading()
     {
         return _('Waiting Income');
+    }
+
+    public function functionName($param)
+    {
+        ['datSplat lte \''.\FlexiPeeHP\FlexiBeeRW::dateToFlexiDate(new \DateTime()).'\' AND (stavUhrK is null OR stavUhrK eq \'stavUhr.castUhr\') AND storno eq false'];
     }
 }
--- modules/WaitingPayments.php
+++ modules/WaitingPayments.php
@@ -15,4 +15,10 @@ class WaitingPayments extends \FlexiPeeHP\DigestMail\DigestModule implements \Fl
     {
         return _('We have to pay');
     }
+    
+    public function functionName($param)
+    {
+        ['datSplat lte \''.\FlexiPeeHP\FlexiBeeRW::dateToFlexiDate(new \DateTime()).'\' AND (stavUhrK is null OR stavUhrK eq \'stavUhr.castUhr\') AND storno eq false'];
+    }
+    
 }
