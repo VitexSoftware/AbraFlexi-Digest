2d3077051a8ccfa8149710f8ab05ce4f0254f818
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-03-30 03:22:53 +0200

    Multicolapse
    Co-authored-by: Borbina <Borbina@users.noreply.github.com>

--- src/modules/WaitingIncome.php
+++ src/modules/WaitingIncome.php
@@ -9,7 +9,8 @@
  *
  * @author vitex
  */
-class WaitingIncome extends \AbraFlexi\Digest\DigestModule implements \AbraFlexi\Digest\DigestModuleInterface {
+class WaitingIncome extends \AbraFlexi\Digest\DigestModule implements \AbraFlexi\Digest\DigestModuleInterface
+{
 
     /**
      * Column used to filter by date
@@ -17,22 +18,33 @@ class WaitingIncome extends \AbraFlexi\Digest\DigestModule implements \AbraFlexi
      */
     public $timeColumn = 'datSplat';
 
-    public function dig() {
+    public function dig()
+    {
         $totals = [];
         $checker = new \AbraFlexi\FakturaVydana();
-        $outInvoices = $checker->getColumnsFromAbraFlexi(['kod', 'firma', 'sumCelkem',
-            'sumCelkemMen',
-            'mena'],
-                array_merge($this->condition,
-                        ["(stavUhrK is null OR stavUhrK eq 'stavUhr.castUhr')",
-                            'storno' => false]));
+        $outInvoices = $checker->getColumnsFromAbraFlexi(
+            [
+                'kod', 'firma', 'sumCelkem',
+                'sumCelkemMen',
+                'mena'
+            ],
+            array_merge(
+                $this->condition,
+                [
+                    "(stavUhrK is null OR stavUhrK eq 'stavUhr.castUhr')",
+                    'storno' => false
+                ]
+            )
+        );
 
         if (empty($outInvoices)) {
             $this->addItem(_('none'));
         } else {
             $adreser = new AbraFlexi\Adresar(null, ['offline' => 'true']);
-            $invTable = new \AbraFlexi\Digest\Table([_('Position'), _('Code'), _('Partner'),
-                _('Amount')]);
+            $invTable = new \AbraFlexi\Digest\Table([
+                _('Position'), _('Code'), _('Partner'),
+                _('Amount')
+            ]);
             $pos = 0;
 
             foreach ($outInvoices as $outInvoiceData) {
@@ -42,10 +54,14 @@ class WaitingIncome extends \AbraFlexi\Digest\DigestModule implements \AbraFlexi
 
                 $invTable->addRowColumns([
                     ++$pos,
-                    new \Ease\Html\ATag($checker->getApiUrl(),
-                            $outInvoiceData['kod']),
-                    new \Ease\Html\ATag($adreser->getApiUrl(),
-                            empty($outInvoiceData['firma']) ? '' : $outInvoiceData['firma@showAs']),
+                    new \Ease\Html\ATag(
+                        $checker->getApiUrl(),
+                        $outInvoiceData['kod']
+                    ),
+                    new \Ease\Html\ATag(
+                        $adreser->getApiUrl(),
+                        empty($outInvoiceData['firma']) ? '' : $outInvoiceData['firma@showAs']
+                    ),
                     (($currency != 'CZK') ? $outInvoiceData['sumCelkemMen'] : $outInvoiceData['sumCelkem']) . ' ' . $currency
                 ]);
 
@@ -57,17 +73,17 @@ class WaitingIncome extends \AbraFlexi\Digest\DigestModule implements \AbraFlexi
             }
 
             $currDiv = new \Ease\Html\DivTag();
-            
-                        foreach ($totals as $currency => $amount) {
+
+            foreach ($totals as $currency => $amount) {
                 $currDiv->addItem(new \Ease\Html\DivTag(self::formatCurrency($amount) . '&nbsp;' . $currency));
             }
-            $this->addItem($this->cardBody(new Ease\Html\H3Tag(_('Total')),[$invTable, $currDiv]));
+            $this->addItem($this->cardBody([new Ease\Html\H3Tag(_('Total') ),$invTable, $currDiv]));
         }
         return !empty($outInvoices);
     }
 
-    public function heading() {
+    public function heading()
+    {
         return _('Waiting Income');
     }
-
 }
